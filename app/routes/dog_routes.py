40dbf2401adb75e566bd51a50ab77af2be4b45fa
Trenisha Goslee <60710670+tgoslee@users.noreply.github.com>
2022-05-05 08:31:16 -0400

    changes to dog class

--- app/routes/dog_routes.py
+++ app/routes/dog_routes.py
@@ -1,26 +1,26 @@
 from flask import Blueprint, jsonify, abort, make_response
+from ..models.dog import Dog
+# class Dog:
+#     def __init__(self, id, name, breed, chip):
+#         self.id = id
+#         self.name = name
+#         self.breed = breed
+#         self.chip = chip
 
-class Dog:
-    def __init__(self, id, name, breed, chip):
-        self.id = id
-        self.name = name
-        self.breed = breed
-        self.chip = chip
+#     def to_dict(self):
+#         return dict(
+#             id=self.id,
+#             name=self.name,
+#             breed=self.breed,
+#             chip=self.chip,
+#         )
 
-    def to_dict(self):
-        return dict(
-            id=self.id,
-            name=self.name,
-            breed=self.breed,
-            chip=self.chip,
-        )
-
-dogs = [
-    Dog(1, "Fido", "shiba inu", "36wkj6w5jh56j"),
-    Dog(2, "Luna", "corgi", "36wkj6w5jh56j"),
-    Dog(3, "Kuro", "husky","36wkj6w5jh56j"),
-    Dog(4, "Max", "lab","36wkj6w5jh56j")
-]
+# dogs = [
+#     Dog(1, "Fido", "shiba inu", "36wkj6w5jh56j"),
+#     Dog(2, "Luna", "corgi", "36wkj6w5jh56j"),
+#     Dog(3, "Kuro", "husky","36wkj6w5jh56j"),
+#     Dog(4, "Max", "lab","36wkj6w5jh56j")
+# ]
 
 bp = Blueprint("dogs", __name__, url_prefix="/dogs")
 
@@ -28,20 +28,21 @@ bp = Blueprint("dogs", __name__, url_prefix="/dogs")
 
 @bp.route("", methods=("GET",))
 def index_dogs():
+    dogs = Dog.query.all()
+
     result_list = [dog.to_dict() for dog in dogs]
 
     return jsonify(result_list)
 
-def validate_dog(id):
+def get_dog_record_by_id(id):
     try:
         id = int(id)
     except ValueError:
         abort(make_response(jsonify(dict(details=f"invalid id: {id}")), 400))
 
-    for dog in dogs:
-        if dog.id == id:
-            # return the dog
-            return dog
+    dog = Dog.query.get(id)
+    if dog:
+        return dog
 
     # no dog found
     abort(make_response(jsonify(dict(details=f"dog id {id} not found")), 404))    
@@ -49,5 +50,5 @@ def validate_dog(id):
 # GET /dogs/id
 @bp.route("/<id>", methods=("GET",))
 def get_dog(id):
-    dog = validate_dog(id)
+    dog = get_dog_record_by_id(id)
     return jsonify(dog.to_dict())
